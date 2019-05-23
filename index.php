49b4007bab45f316f3c55a77195f6c2b066caab2
zeidane97 <agbzeidane18@gmail.com>
2019-05-23 12:55:56 +0100

    ma page de connexion

--- index.php
+++ index.php
@@ -4,11 +4,29 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="assets/css/index.css">
         <title>Document</title>
     </head>
     <body>
         <header>
-            <h1>BIENVENUE</h1>
+            <h1 >BIENVENUE</h1>
         </header>
+        <div id="main">
+            <form action="#">
+                <img src="assets/img/logo.png" alt="">
+                <div>
+                    <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo">
+                </div>
+                <div id="champPass">
+                    <input type="password" name="pass" id="pass" placeholder="Password">
+                    <input type="submit" value="Login" id="login" name="bt-login"> 
+                </div>
+            </form>
+            <div id="compte">
+                <a href="#">Creer un compte</a>
+                <a href="#">Anonyme</a>
+            </div>
+            <p>Environnment de tchabot personnalisé pour tchater avec vos proches amis et connaissances en toute sécurité, simplement créer un compte et découvrer l'univers de livetchat</p>
+        </div>
     </body>
 </html>
